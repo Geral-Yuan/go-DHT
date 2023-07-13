4da8b183d696f57f75ec5d3a70c42430d9da9817
Yuan Jiale <yjl2030906598@sjtu.edu.cn>
2023-07-13 16:05:05 +0800

    adjust file structure: move helper functions from node.go to node_access.go and print_info.go

--- chord/node.go
+++ chord/node.go
@@ -1,10 +1,8 @@
 package chord
 
 import (
-	"fmt"
 	"math/big"
 	"os"
-	"strings"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -225,85 +223,6 @@ func calcID(id *big.Int, i uint) *big.Int {
 	return new(big.Int).Mod(new(big.Int).Add(id, offset), mod)
 }
 
-func (node *Node) get_successor(res *SingleNode) error {
-	var i uint
-	for i = 0; i < successorListLength; i++ {
-		node.successorListLock.RLock()
-		*res = node.successorList[i]
-		node.successorListLock.RUnlock()
-		if res.Addr != "" && node.Ping(res.Addr) {
-			return nil
-		} else {
-			logrus.Infof("Info <func get_successor()> node [%s] fail to ping node [%s]", node.getPort(), res.getPort())
-		}
-	}
-	*res = SingleNode{"", nil}
-	return fmt.Errorf("no node in [%s]'s successorList is online", node.getPort())
-}
-
-func (node *Node) get_successorList(res *[successorListLength]SingleNode) error {
-	node.successorListLock.RLock()
-	*res = node.successorList
-	node.successorListLock.RUnlock()
-	return nil
-}
-
-func (node *Node) get_predecessor(res *SingleNode) error {
-	node.predecessorLock.RLock()
-	*res = node.predecessor
-	node.predecessorLock.RUnlock()
-	if node.Ping(res.Addr) {
-		return nil
-	} else {
-		return fmt.Errorf("node [%s]'s predecessor offline", node.getPort())
-	}
-}
-
-func (node *Node) get_finger_i(i uint, res *SingleNode) error {
-	node.fingerLock.RLock()
-	*res = node.finger[i]
-	node.fingerLock.RUnlock()
-	return nil
-}
-
-func (node *Node) set_successor(n *SingleNode) error {
-	node.successorListLock.Lock()
-	node.successorList[0] = *n
-	node.successorListLock.Unlock()
-	return nil
-}
-
-func (node *Node) add_successor(suc SingleNode) error {
-	if !node.Ping(suc.Addr) {
-		return fmt.Errorf("find [%s] offline when adding it to node [%s]'s successorList", suc.getPort(), node.getPort())
-	}
-	node.set_successor(&suc)
-	node.set_finger_i(0, &suc)
-	var suc_successorList [successorListLength]SingleNode
-	node.RemoteCall("tcp", suc.Addr, "RPC_Node.Get_successorList", struct{}{}, &suc_successorList)
-	var i uint
-	node.successorListLock.Lock()
-	for i = 1; i < successorListLength; i++ {
-		node.successorList[i] = suc_successorList[i-1]
-	}
-	node.successorListLock.Unlock()
-	return nil
-}
-
-func (node *Node) set_predecessor(n *SingleNode) error {
-	node.predecessorLock.Lock()
-	node.predecessor = *n
-	node.predecessorLock.Unlock()
-	return nil
-}
-
-func (node *Node) set_finger_i(i uint, n *SingleNode) error {
-	node.fingerLock.Lock()
-	node.finger[i] = *n
-	node.fingerLock.Unlock()
-	return nil
-}
-
 func (node *Node) clear() {
 	node.dataLock.Lock()
 	node.data = make(map[string]string)
@@ -470,36 +389,4 @@ func (node *Node) maintainChord() {
 
 func (node *Node) isOnline() error {
 	return nil
-}
-
-// helper functions for debugging
-
-func (node *Node) PrintNodeInfo() {
-	var sucList [successorListLength]SingleNode
-	var pre SingleNode
-	node.successorListLock.RLock()
-	node.get_successorList(&sucList)
-	node.successorListLock.RUnlock()
-	// node.get_successor(&suc)
-	node.get_predecessor(&pre)
-	logrus.Infof("node [%s]: successorList [%s][%s][%s][%s][%s], predecessor [%s]", node.getPort(), sucList[0].getPort(), sucList[1].getPort(), sucList[2].getPort(), sucList[3].getPort(), sucList[4].getPort(), pre.getPort())
-}
-
-func (node *Node) PrintFingers() {
-	var f SingleNode
-	var i uint
-	for i = 0; i < M; i++ {
-		node.get_finger_i(i, &f)
-		logrus.Infof(">>>>>>>>>> finger [%d]: [%s] <<<<<<<<<<", i, f.getPort())
-	}
-}
-
-// get port number
-
-func (node *SingleNode) getPort() string {
-	return getPortFromIP(node.Addr)
-}
-
-func getPortFromIP(ip string) string {
-	return ip[strings.Index(ip, ":")+1:]
 }
